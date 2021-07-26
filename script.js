6bd4caeb54b13e92aafe5819b629408437f3f50d
Joseph <serinojoseph@gmail.com>
2021-07-26 07:23:57 -0700

    final update

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-//WHY DOES IT MATTER THAT MY FUNCTION ACCEPTS JSON? TALK ABOUT PARSE AND STRINGIFY? 
-//DO THESE OBJECTS REALLY NEED TO CONTAIN STRINGS? WHY OR WHY NOT?
-//TALK ABOUT LOOPING THROUGH OBJECTS.
-
 let arr = [{"fname":"esme", "lname":"sanchez", "id":1892038, "isTherapist":true},
 
 {"fname":"rick", "lname":"jeps", "id":1894024, "isTherapist":false},
@@ -18,12 +13,10 @@ render(arr)
 function render(arr) {
   let mainDiv = document.getElementById("mainDiv")
 
-  //IS THIS THE OPTIMUM TYPE OF LOOP?
   for (let i = 0; i < arr.length; i++) {
     let pTag = document.createElement("p")
     mainDiv.appendChild(pTag)
     
-    //CAN WE COMBINE THESE EVENT LISTENERS?
     pTag.addEventListener("mouseover", (event) => 
       event.target.style.backgroundColor = "lightgrey"
     )
@@ -32,7 +25,6 @@ function render(arr) {
       event.target.style.backgroundColor = "white"
     )
     
-    //CAN WE SIMPLIFY THIS?
     pTag.innerHTML = `${arr[i].lname.charAt(0).toUpperCase()}${arr[i].lname.slice(1)}, ${arr[i].fname.charAt(0).toUpperCase()}${arr[i].fname.slice(1)}`
   }
 }
