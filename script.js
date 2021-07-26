ffade7d2224974118352b836ec332d56bc658206
Joseph <serinojoseph@gmail.com>
2021-07-25 22:52:37 -0700

    updated css

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+//WHY DOES IT MATTER THAT MY FUNCTION ACCEPTS JSON? TALK ABOUT PARSE AND STRINGIFY? 
+//DO THESE OBJECTS REALLY NEED TO CONTAIN STRINGS? WHY OR WHY NOT?
+//TALK ABOUT LOOPING THROUGH OBJECTS.
+
 let arr = [{"fname":"esme", "lname":"sanchez", "id":1892038, "isTherapist":true},
 
 {"fname":"rick", "lname":"jeps", "id":1894024, "isTherapist":false},
@@ -13,10 +18,12 @@ render(arr)
 function render(arr) {
   let mainDiv = document.getElementById("mainDiv")
 
+  //IS THIS THE OPTIMUM TYPE OF LOOP?
   for (let i = 0; i < arr.length; i++) {
     let pTag = document.createElement("p")
     mainDiv.appendChild(pTag)
-
+    
+    //CAN WE COMBINE THESE EVENT LISTENERS?
     pTag.addEventListener("mouseover", (event) => 
       event.target.style.color = "red"
     )
@@ -25,9 +32,8 @@ function render(arr) {
       event.target.style.color = "black"
     )
     
+    //CAN WE SIMPLIFY THIS?
     pTag.innerHTML = `${arr[i].lname.charAt(0).toUpperCase()}${arr[i].lname.slice(1)}, ${arr[i].fname.charAt(0).toUpperCase()}${arr[i].fname.slice(1)}`
   }
 }
-
-//I NEED TO CENTER THE ELEMENTS AND ADD PADDING AS NOTED IN THE INSTRUCTIONS.
 
