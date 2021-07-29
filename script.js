59307820a1778611c80f5a7c549eb9c090728bcd
Joseph <serinojoseph@gmail.com>
2021-07-28 22:47:16 -0700

    done

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-let arr = [{"fname":"esme", "lname":"sanchez", "id":1892038, "isTherapist":true},
+async function main() {
+	const res = await fetch(`./people.json`);
+	const people = await res.json();
+	const mainDiv = document.getElementById(`mainDiv`);
 
-{"fname":"rick", "lname":"jeps", "id":1894024, "isTherapist":false},
-
-{"fname":"angie", "lname":"lange", "id":1982403, "isTherapist":false},
-
-{"fname":"renat", "lname":"gros", "id":1894039, "isTherapist":true},
-
-{"fname":"bettino", "lname":"yang", "id":3789204, "isTherapist":true}]
-
-render(arr)
-
-function render(arr) {
-  let mainDiv = document.getElementById("mainDiv")
-
-  for (let i = 0; i < arr.length; i++) {
-    let pTag = document.createElement("p")
-    mainDiv.appendChild(pTag)
-    
-    pTag.innerHTML = `${arr[i].lname.charAt(0).toUpperCase()}${arr[i].lname.slice(1)}, ${arr[i].fname.charAt(0).toUpperCase()}${arr[i].fname.slice(1)}`
-  }
+	people.forEach((person) => {
+		const pTag = document.createElement(`p`);
+		pTag.innerText = `${person.lname}, ${person.fname}`;
+		mainDiv.appendChild(pTag);
+	});
 }
+
+main();
 
